1678f6fcbf1bd3861ca8aaf60d7ba28c1cbe4ef8
Joe Thompson <jthompson7@blueorigin.com>
2023-06-13 13:16:56 -0700

    Add blog admin project.

---
+++ blog-site/app/models/post.js
@@ -0,0 +1,9 @@
+export default class Post {
+    constructor(title, publishDate, author, body, comments) {
+        this.title = title
+        this.publishDate = publishDate
+        this.author = author
+        this.body = body
+        this.comments = comments
+    }
+}
